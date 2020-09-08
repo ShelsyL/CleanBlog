2d5b491b9ed1fbe424f91704e093c11bb4f3b122
Chelsea <chelsealouis.graphiste@gmail.com>
2020-09-08 15:10:33 +0200

    Arborescence MVC
    Création de la structure MVC, fichiers vide

---
+++ app/config/parametres.php
@@ -0,0 +1,13 @@
+<?php
+/*
+  ./app/config/params.php
+*/
+
+// // PARAMETRES DE CONNEXION
+//   define('DBHOTE', '127.0.0.1:3306'); // Dépend de notre localhost
+//   define('DBNAME', 'cleanblog_2017'); // A changer en fonction du projet
+//   define('DBUSER', 'root');
+//   define('DBPWD', 'root');
+//
+// // INITIALISATION DES ZONES DYNAMIQUES
+// $content = '';
